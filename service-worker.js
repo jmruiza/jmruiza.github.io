4033656861fd65d67a3cd37883af64fb7dee4f5a
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-06-01 19:16:25 +0000

    Deploy jmruiza/jmruiza.github.io to github.com/jmruiza/jmruiza.github.io.git:master

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.054774adbe886ee6e3c29227ef1745b5.js"
+  "/precache-manifest.7df17bd0a2fda8c7042f2ef74d65b174.js"
 );
 
 self.addEventListener('message', (event) => {
